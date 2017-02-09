c427d679e808425cd32921fc9dea2f56688544a1
Jan <janvonpichowski@gmail.com>
2017-02-09 14:09:39 +0100

    Added some controllers and components

---
+++ src/main/java/com/jvpichowski/jme3/es/bullet/RigidBodyContainer.java
@@ -0,0 +1,49 @@
+package com.jvpichowski.jme3.es.bullet;
+
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jvpichowski.jme3.es.bullet.components.CollisionShape;
+import com.jvpichowski.jme3.es.bullet.components.RigidBody;
+import com.simsilica.es.Entity;
+import com.simsilica.es.EntityContainer;
+import com.simsilica.es.EntityData;
+
+/**
+ * Created by Jan on 08.02.2017.
+ */
+final class RigidBodyContainer extends EntityContainer<PhysicsRigidBody> {
+
+    private PhysicsSpace physicsSpace;
+
+    public RigidBodyContainer(EntityData ed, PhysicsSpace physicsSpace) {
+        super(ed, RigidBody.class, CollisionShape.class);
+        this.physicsSpace = physicsSpace;
+    }
+
+    @Override
+    protected PhysicsRigidBody addObject(Entity e) {
+        RigidBody rigidBodyInfo = e.get(RigidBody.class);
+        CollisionShape collisionShapeInfo = e.get(CollisionShape.class);
+        PhysicsRigidBody rigidBody = new PhysicsRigidBody(collisionShapeInfo.getCollisionShape(), rigidBodyInfo.getMass());
+        rigidBody.setMass(rigidBodyInfo.getMass());
+        rigidBody.setKinematic(rigidBodyInfo.isKinematic());
+        rigidBody.setUserObject(e.getId());
+        physicsSpace.add(rigidBody);
+        return rigidBody;
+    }
+
+    @Override
+    protected void updateObject(PhysicsRigidBody object, Entity e) {
+        RigidBody rigidBodyInfo = e.get(RigidBody.class);
+        CollisionShape collisionShapeInfo = e.get(CollisionShape.class);
+        object.setKinematic(rigidBodyInfo.isKinematic());
+        object.setMass(rigidBodyInfo.getMass());
+        object.setCollisionShape(collisionShapeInfo.getCollisionShape());
+//            object.setUserObject(e.getId()); already set?
+    }
+
+    @Override
+    protected void removeObject(PhysicsRigidBody object, Entity e) {
+        physicsSpace.remove(object);
+    }
+}
