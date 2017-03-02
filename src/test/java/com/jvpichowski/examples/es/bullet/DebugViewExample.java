0f50de2aa52ab510638f10cd1764afbe41726fb9
Jan von Pichowski <janvonpichowski@gmail.com>
2017-03-02 19:17:42 +0100

    Added DebugViewState

---
+++ src/test/java/com/jvpichowski/examples/es/bullet/DebugViewExample.java
@@ -0,0 +1,58 @@
+package com.jvpichowski.examples.es.bullet;
+
+import com.jme3.app.FlyCamAppState;
+import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jvpichowski.jme3.es.bullet.CollisionShapeFactory;
+import com.jvpichowski.jme3.es.bullet.components.BoxShape;
+import com.jvpichowski.jme3.es.bullet.components.CustomShape;
+import com.jvpichowski.jme3.es.bullet.components.PhysicsPosition;
+import com.jvpichowski.jme3.es.bullet.components.RigidBody;
+import com.jvpichowski.jme3.states.ESBulletState;
+import com.jvpichowski.jme3.states.bullet.DebugViewState;
+import com.simsilica.es.EntityData;
+import com.simsilica.es.EntityId;
+import com.simsilica.es.base.DefaultEntityData;
+
+/**
+ * Example No. 5
+ */
+public class DebugViewExample extends SimpleApplication {
+
+    public static void main(String[] args) {
+        EntityData entityData = new DefaultEntityData();
+        DebugViewExample app = new DebugViewExample(entityData);
+        app.start();
+    }
+
+    private final EntityData entityData;
+
+    public DebugViewExample(EntityData entityData){
+        super(new FlyCamAppState(), new ESBulletState(entityData));
+        this.entityData = entityData;
+    }
+
+    @Override
+    public void simpleInitApp() {
+        //create a new debug view state
+        //you have to add the same CustomShapeFactory which you have added to the BulletSystem
+        //if no factory is given to the constructor the default factory will be used (CollisionShapeFactory)
+        DebugViewState debugView = new DebugViewState(entityData, getRootNode());
+        getStateManager().attach(debugView);
+
+        EntityId sphere = entityData.createEntity();
+        entityData.setComponents(sphere,
+                new CustomShape(new SphereCollisionShape(0.5f)),
+                new RigidBody(false, 10),
+                new PhysicsPosition(new Vector3f(0, 15, 0), Quaternion.DIRECTION_Z.clone()));
+
+        EntityId box = entityData.createEntity();
+        entityData.setComponents(box,
+                new BoxShape(new Vector3f(5f, 0.1f, 5f)),
+                new RigidBody(false, 0),
+                new PhysicsPosition(new Vector3f(), new Quaternion()));
+    }
+
+}
