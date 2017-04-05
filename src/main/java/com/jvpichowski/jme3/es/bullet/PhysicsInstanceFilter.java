b5b66aa6cc62a8c377b18affca213e7981e0ca8c
Jan <janvonpichowski@gmail.com>
2017-04-05 21:13:43 +0200

    add helper method to instance filter

--- src/main/java/com/jvpichowski/jme3/es/bullet/PhysicsInstanceFilter.java
+++ src/main/java/com/jvpichowski/jme3/es/bullet/PhysicsInstanceFilter.java
@@ -3,6 +3,8 @@ package com.jvpichowski.jme3.es.bullet;
 import com.jvpichowski.jme3.es.bullet.components.PhysicsInstance;
 import com.simsilica.es.ComponentFilter;
 import com.simsilica.es.EntityComponent;
+import com.simsilica.es.EntityData;
+import com.simsilica.es.EntitySet;
 
 /**
  * This system isn't fully integrated. Later it will be possible to have multiple
@@ -25,5 +27,13 @@ public final class PhysicsInstanceFilter implements ComponentFilter<PhysicsInsta
     @Override
     public boolean evaluate(EntityComponent c) {
         return ((PhysicsInstance)c).getInstance() == instance;
+    }
+
+    public static EntitySet getEntities(EntityData entityData, PhysicsInstanceFilter filter, Class<? extends EntityComponent> components){
+        if(filter == null){
+            return entityData.getEntities(components);
+        }else {
+            return entityData.getEntities(filter, filter.getComponentType(), components);
+        }
     }
 }
