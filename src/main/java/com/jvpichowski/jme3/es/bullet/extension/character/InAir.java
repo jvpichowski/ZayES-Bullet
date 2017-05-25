adc1e4dbe7c2ba25e5cc1d0f3d6ab64c7da666f7
Jan von Pichowski <janvonpichowski@gmail.com>
2017-05-25 20:36:16 +0200

    removed old character

--- src/main/java/com/jvpichowski/jme3/es/bullet/extension/character/InAir.java
+++
@@ -1,9 +0,0 @@
-package com.jvpichowski.jme3.es.bullet.extension.character;
-
-import com.simsilica.es.EntityComponent;
-
-/**
- * An entity has no contact to the ground
- * if this component is attached to it
- */
-final class InAir implements EntityComponent {}
